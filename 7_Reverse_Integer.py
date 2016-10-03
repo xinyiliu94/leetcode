39404227ea8fc55c804f70978bf185ae305864d8
XINYI LIU <xil572@eng.ucsd.edu>
2016-10-02 18:00:09 -0800

    Update 7_Reverse_Integer.py

--- 7_Reverse_Integer.py
+++ 7_Reverse_Integer.py
@@ -27,3 +27,18 @@ class Solution(object):
             reverse_num = 0
         
         return reverse_num
+    
+class Solution2(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 0:
+            return -self.reverse(-x)
+
+        result = 0
+        while x:
+            result = result * 10 + x % 10
+            x /= 10
+        return result if result <= 0x7fffffff else 0
