af450b57a11385dd3b54600a64364bbf78ac267a
XINYI LIU <xil572@eng.ucsd.edu>
2016-10-08 21:25:26 -0700

    Update 189_Rotate_Array.py

--- 189_Rotate_Array.py
+++ 189_Rotate_Array.py
@@ -4,3 +4,24 @@ For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,
 
 Note:
 Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.'''
+
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
+        nums[:] = nums[n-k:] + nums[:n-k]
+
+      
+ class Solution(object):
+    def rotate(self, nums,k):
+      n=len(nums)
+        if k!=0:
+            k=k%n
+            for i in xrange(k):
+                nums.insert(0,nums.pop())
